1d0959f470511b8650cf6a5d52f50872352640de
atieh <a.sorahitalab@gmail.com>
2025-08-20 12:32:12 +0330

    change styling selected pizza section

--- pages/sections/main/selected-pizza.view.php
+++ pages/sections/main/selected-pizza.view.php
@@ -1,91 +1,107 @@
 <section class="selected-pizza">
 <div class="container">
     <h3 class="title-section">SELECTED PIZZAS</h3>
-    <div class="products">
-        <div class="product-box wow fadeInLeft">
-            <article class="product">
-                <div class="product-figure">
-                    <img src="assets/img/product/product-1.png" alt="">
-                </div>
-                <div class="product-rating">
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star text-gray"><i class="fi fi-star"></i></span>
-                </div>
-                <h6 class="product-title">
-                    Margherita Pizza
-                </h6>
-                <div class="product-price-wrap">
-                    <div class="product-price">$24.00</div>
-                </div>
-                <div class="product-button"></div>
-            </article>
+    <div class="row products">
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="product-box wow fadeInLeft">
+                <div class="cover"></div>
+                <article class="product">
+                    <div class="product-figure">
+                        <img src="assets/img/product/product-1.png" alt="">
+                    </div>
+                    <div class="product-rating">
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star text-gray"><i class="fi fi-star"></i></span>
+                    </div>
+                    <h6 class="product-title">
+                        Margherita Pizza
+                    </h6>
+                    <div class="product-price-wrap">
+                        <div class="product-price">$24.00</div>
+                    </div>
+                    <div class="product-button">
+                        <div class="button-wrap">
+                            <div class="content-original">Add to cart</div>
+                        </div>
+                        <div class="button-wrap">
+                            <div class="content-original">View Product</div>
+                        </div>
+                    </div>
+                </article>
+            </div>
         </div>
-        <div class="product-box wow fadeInLeft">
-            <article class="product">
-                <div class="product-figure">
-                    <img src="assets/img/product/product-2.png" alt="">
-                </div>
-                <div class="product-rating">
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star"><i class="fi fi-star"></i></span>
-                </div>
-                <h6 class="product-title">
-                    Mushroom Pizza
-                </h6>
-                <div class="product-price-wrap">
-                    <div class="product-price">$24.00</div>
-                </div>
-                <div class="product-button"></div>
-            </article>
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="product-box wow fadeInLeft">
+                <article class="product">
+                    <div class="product-figure">
+                        <img src="assets/img/product/product-2.png" alt="">
+                    </div>
+                    <div class="product-rating">
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star"><i class="fi fi-star"></i></span>
+                    </div>
+                    <h6 class="product-title">
+                        Mushroom Pizza
+                    </h6>
+                    <div class="product-price-wrap">
+                        <div class="product-price">$24.00</div>
+                    </div>
+                    <div class="product-button"></div>
+                </article>
+            </div>
         </div>
-        <div class="product-box wow fadeInLeft">
-            <article class="product">
-                <div class="product-figure">
-                    <img src="assets/img/product/product-3.png" alt="">
-                </div>
-                <div class="product-rating">
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star text-gray"><i class="fi fi-star"></i></span>
-                </div>
-                <h6 class="product-title">
-                    Hawaiian Pizza
-                </h6>
-                <div class="product-price-wrap">
-                    <div class="product-price">$24.00</div>
-                </div>
-                <div class="product-button"></div>
-            </article>
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="product-box wow fadeInLeft">
+                <article class="product">
+                    <div class="product-figure">
+                        <img src="assets/img/product/product-3.png" alt="">
+                    </div>
+                    <div class="product-rating">
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star text-gray"><i class="fi fi-star"></i></span>
+                    </div>
+                    <h6 class="product-title">
+                        Hawaiian Pizza
+                    </h6>
+                    <div class="product-price-wrap">
+                        <div class="product-price">$24.00</div>
+                    </div>
+                    <div class="product-button"></div>
+                </article>
+            </div>
         </div>
-        <div class="product-box wow fadeInLeft">
-            <article class="product">
-                <div class="product-figure">
-                    <img src="assets/img/product/product-4.png" alt="">
-                </div>
-                <div class="product-rating">
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star"><i class="fi fi-star"></i></span>
-                    <span class="star"><i class="fi fi-star"></i></span>
-                </div>
-                <h6 class="product-title">
-                    Pesto Pizza
-                </h6>
-                <div class="product-price-wrap">
-                    <div class="product-price product-price-old">$40.00</div>
-                    <div class="product-price">$24.00</div>
-                </div>
-                <div class="product-button"></div>
-            </article>
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+            <div class="product-box wow fadeInLeft">
+                <article class="product">
+                    <div class="product-figure">
+                        <img src="assets/img/product/product-4.png" alt="">
+                    </div>
+                    <div class="product-rating">
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star"><i class="fi fi-star"></i></span>
+                        <span class="star"><i class="fi fi-star"></i></span>
+                    </div>
+                    <h6 class="product-title">
+                        Pesto Pizza
+                    </h6>
+                    <div class="product-price-wrap">
+                        <div class="product-price product-price-old">$40.00</div>
+                        <div class="product-price">$24.00</div>
+                    </div>
+                    <div class="product-button"></div>
+                </article>
+            </div>
         </div>
     </div>
 </div>
