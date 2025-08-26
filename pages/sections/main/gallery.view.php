d696882f15825b2d5059768b8b654daf7993d17a
atieh <a.sorahitalab@gmail.com>
2025-08-26 12:05:51 +0330

    change gallery section

--- pages/sections/main/gallery.view.php
+++ pages/sections/main/gallery.view.php
@@ -5,7 +5,7 @@
 <section class="gallery">
     <div class="gallery__inner">
         <div class="gallery__inner-img gallery-1"></div>
-        <img class="gallery__inner-img img1" src="assets/img/gallery/gallery-1.jpg" alt="">
+        <!--<img class="gallery__inner-img img1" src="assets/img/gallery/gallery-1.jpg" alt="">-->
         <div class="hidden__desc">
             <h3 class="gallery__inner-title">BEST INGREDIENTS</h3>
             <h4 class="gallery__inner-description">TASTY PIZZA</h4>
@@ -13,7 +13,7 @@
     </div>
     <div class="gallery__inner">
         <div class="gallery__inner-img gallery-2"></div>
-        <img class="gallery__inner-img img2" src="assets/img/gallery/gallery-2.jpg" alt="">
+       <!-- <img class="gallery__inner-img img2" src="assets/img/gallery/gallery-2.jpg" alt="">-->
         <div class="hidden__desc">
             <h3 class="gallery__inner-title">COMFORTABLE INTERIOR</h3>
             <h4 class="gallery__inner-description">MODERN DESIGN</h4>
@@ -21,7 +21,7 @@
     </div>
     <div class="gallery__inner">
         <div class="gallery__inner-img gallery-3"></div>
-        <img class="gallery__inner-img img3" src="assets/img/gallery/gallery-3.jpg" alt="">
+        <!--<img class="gallery__inner-img img3" src="assets/img/gallery/gallery-3.jpg" alt="">-->
         <div class="hidden__desc">
             <h3 class="gallery__inner-title">QUALITY DISHWARE</h3>
             <h4 class="gallery__inner-description">TOP-NOTCH UTENSILS</h4>
@@ -29,7 +29,7 @@
     </div>
     <div class="gallery__inner">
         <div class="gallery__inner-img gallery-4"></div>
-        <img class="gallery__inner-img img4" src="assets/img/gallery/gallery-4.jpg" alt="">
+        <!--<img class="gallery__inner-img img4" src="assets/img/gallery/gallery-4.jpg" alt="">-->
         <div class="hidden__desc">
             <h3 class="gallery__inner-title">EXOTIC SALADS</h3>
             <h4 class="gallery__inner-description">SUMMER TASTE</h4>
@@ -37,7 +37,7 @@
     </div>
     <div class="gallery__inner">
         <div class="gallery__inner-img gallery-5"></div>
-        <img class="gallery__inner-img img5" src="assets/img/gallery/gallery-5.jpg" alt="">
+       <!-- <img class="gallery__inner-img img5" src="assets/img/gallery/gallery-5.jpg" alt="">-->
         <div class="hidden__desc">
             <h3 class="gallery__inner-title">REFRESHING COCKTAILS</h3>
             <h4 class="gallery__inner-description">EXCLUSIVE SELECTION</h4>
@@ -45,7 +45,7 @@
     </div>
     <div class="gallery__inner">
         <div class="gallery__inner-img gallery-6"></div>
-        <img class="gallery__inner-img img6" src="assets/img/gallery/gallery-6.jpg" alt="">
+        <!--<img class="gallery__inner-img img6" src="assets/img/gallery/gallery-6.jpg" alt="">-->
         <div class="hidden__desc">
             <h3 class="gallery__inner-title">ALL TYPES OF PIZZA</h3>
             <h4 class="gallery__inner-description">SPECIAL RECIPES</h4>
@@ -53,7 +53,7 @@
     </div>
     <div class="gallery__inner">
         <div class="gallery__inner-img gallery-7"></div>
-        <img class="gallery__inner-img img7" src="assets/img/gallery/gallery-7.jpg" alt="">
+        <!--<img class="gallery__inner-img img7" src="assets/img/gallery/gallery-7.jpg" alt="">-->
         <div class="hidden__desc">
             <h3 class="gallery__inner-title">DIVERSE MENU</h3>
             <h4 class="gallery__inner-description">PICK YOUR FAVORITE DISH</h4>
